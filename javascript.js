b339433ff49d04807fc996fa4e33f1d299c2d58e
rloftus215 <rloftus215@gmail.com>
2024-06-13 07:39:12 +0100

    add fucntion to clear output when clear button pressed

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const buttonContainer = document.querySelector('#button-container');
 const buttons = buttonContainer.querySelectorAll('.button');
 const output = document.querySelector('#output');
+const clearButton = document.querySelector('.clear-button');
 
 let displayValue = '';
 
@@ -10,3 +11,10 @@ buttons.forEach((button) => {button.addEventListener('click', () => {
     })
 });
  
+function clearOutput() {
+    output.innerText = '';
+    displayValue = '';
+ };
+ 
+ 
+ clearButton.addEventListener('click', clearOutput)
