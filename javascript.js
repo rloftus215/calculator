0712098f7da8055c06b8ae1b24cb24cc5a500415
rloftus215 <rloftus215@gmail.com>
2024-06-13 07:44:30 +0100

    add event listener to operator buttons to change operator value

--- javascript.js
+++ javascript.js
@@ -3,7 +3,13 @@ const buttons = buttonContainer.querySelectorAll('.button');
 const output = document.querySelector('#output');
 const clearButton = document.querySelector('.clear-button');
 
+plusButton.addEventListener('click', () => {operator = '+'});
+subButton.addEventListener('click', () => {operator = '-'});
+multiplyButton.addEventListener('click', () => {operator = 'x'});
+divideButton.addEventListener('click', () => {operator = '/'});
+
 let displayValue = '';
+let operator;
 
 buttons.forEach((button) => {button.addEventListener('click', () => {
     displayValue += `${button.textContent}`;
